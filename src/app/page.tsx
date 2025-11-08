01441f8610d273b1c63cf7903d68c844f93f010e
Arya Athaullah Rafi <aathaullah@student.ciputra.ac.id>
2025-11-08 16:01:46 +0700

    add homepage bagian atas (ARYA)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,89 @@
 import Image from "next/image";
+import Link from "next/link";
+import "./styles/homepage.css";
+import LogoLoop from './components/LogoLoop'
 
 export default function Home() {
+
+  const TEMP_LOGO_PATH = '/Images/logo.png';
+  
+  const partnerLogos = [
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 1",
+    },
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 2",
+    },
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 3",
+    },
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 4",
+    },
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 5",
+    },
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 6",
+    },
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 7",
+    },
+    {
+      src: TEMP_LOGO_PATH,
+      alt: "Partner 8",
+    }
+  ];
+
   return (
-    <main className = "flex bg-white">
-      <div className="m-auto">
-        <Image
-          src="/images/logo.png"
-          alt="Ciputra Color Run Logo"
-          width={300}
-          height={300}
-          className="object-contain"
-        />
+    <main className="bg-white">
+      <div className="home_top">
+        <div className="home_top_content">
+          <img
+            src="/Images/logo.png"
+            alt="Ciputra Color Run Logo"
+            className="home_top_logo"
+          />
+          <h1 className="home_title">CIPUTRA COLOR RUN</h1>
+
+          <Link href="/register" className="home_register_button">
+            REGISTER NOW
+          </Link>
+          
+          <p className="home_description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate quis aperiam voluptatem perferendis eos distinctio voluptatum exercitationem enim, error dolores iure magnam explicabo vel consectetur ullam excepturi recusandae alias tenetur!
+          </p>
+        </div>
       </div>
+
+      <div className="sponsor-container">
+
+        <h1 className="sponsor-title">MAIN SPONSOR</h1>
+
+        <div className="partner-loop-with-borders">
+          <LogoLoop
+            logos={partnerLogos}
+            speed={40}
+            direction="left"
+            logoHeight={120}
+            gap={20}
+            pauseOnHover={true}
+            fadeOut={false}
+            scaleOnHover={false}
+            className="partner-logo-loop"
+          />
+        </div>
+      </div>
+
+
+
     </main>
   );
 }
