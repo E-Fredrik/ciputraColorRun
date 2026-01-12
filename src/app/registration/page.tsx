b57cdec022fdf87f81ab75c05e4433692ef03a3a
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2026-01-12 14:10:26 +0700

    Changed T

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1065,7 +1065,7 @@ export default function RegistrationPage() {
                                         // setRegistrationType("community");
                                         // Redirect to contact person's WhatsApp (Abel)
                                         if (typeof window !== "undefined") {
-                                          const msg = "Halo kak, saya ingin mendaftar Ciputra Color Run sebagai komunitas. Mohon informasinya, Terima kasih";
+                                          const msg = "Halo kak, saya ingin mendaftar Ciputra Color Run sebagai komunitas. Mohon informasinya, terima kasih";
                                           const encoded = encodeURIComponent(msg);
                                           window.location.href = `https://wa.me/62895410319676?text=${encoded}`;
                                         }
