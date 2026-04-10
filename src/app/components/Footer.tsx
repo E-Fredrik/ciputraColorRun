002967b3a0fc4e29dc8e5f1696c5bbef20628a11
aryathaullah <aryaathaullah7@gmail.com>
2026-04-10 16:55:33 +0700

    add jete logo sedang on footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -44,7 +44,8 @@ export default function Footer() {
     "/images/sponsor/sedang/sofresh-logo-sedang.png",
     "/images/sponsor/sedang/soklin-logo-sedang.png",
     "/images/sponsor/sedang/wizz-logo-sedang.png",
-    "/images/sponsor/sedang/wiyung-logo-sedang.png"
+    "/images/sponsor/sedang/wiyung-logo-sedang.png",
+    "/images/sponsor/sedang/jete-logo-sedang.png",
   ];
 
   const sponsorLogosKecil = [
